c1e0c761f1b5fb50af19e3125a8884a0870a9433
Alexandr <107064917+etrehappy@users.noreply.github.com>
2024-07-05 15:35:41 +0300

    extra_task

---
+++ source/cmd_reader.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+#include <thread> // for PrintForCheck()
+#include <chrono> 
+
+#include "m_exceptions.h"
+
+class CommandReader
+{
+public:
+	CommandReader(size_t);
+	~CommandReader() = default;
+
+	void Read(std::istream&);
+	std::vector<std::string>& GetCommands();
+	bool IsEof() const;
+	std::chrono::system_clock::time_point GetBulkTime() const;		
+
+private:
+
+	void ClearState();
+
+	/*!
+	* @brief Задание: Данные подаются на стандартный вход построчно с паузой
+	*	в 1 секунду для визуального контроля.
+	*/
+	void PrintForCheck() const;
+
+	const size_t max_command_number_;
+	size_t command_counter_;
+	std::vector<std::string> container_;
+	std::chrono::system_clock::time_point bulk_time_;
+	bool eof_flag_;
+};
+
+
